7e99cec1533c75d700b80e7228d1db59615514f0
Ali Raza <aliraza.2369196@gmail.com>
2021-07-18 14:14:15 +0500

    'LaravelPassport'

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodosController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,11 @@ use App\Http\Controllers\TodosController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
+
 
 Route::resource('todos', TodosController::class);
 Route::patch('/todosCheckAll', [TodosController::class, 'checkAll']);
