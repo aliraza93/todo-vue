c13ddcb209ea1c7b74f43658b83d782149be614f
Ali Raza <aliraza.2369196@gmail.com>
2021-07-18 14:18:13 +0500

    'PassportAuthentication'

--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,7 @@ axios.defaults.baseURL = 'http://todo-laravel.test/api'
 
 export const store = new Vuex.Store({
     state: {
+        token: localStorage.getItem('access_token') || null,
         todos: [],
         filter: 'all',      
     },
@@ -38,6 +39,10 @@ export const store = new Vuex.Store({
     
         showClearCompletedButton(state) {
             return state.todos.filter(todo => todo.completed).length > 0
+        },
+
+        loggedIn(state) {
+            return state.token != null
         }
     },
 
@@ -80,6 +85,14 @@ export const store = new Vuex.Store({
 
         retrieveTodos(state, todos) {
             state.todos = todos
+        },
+
+        retrieveToken(state, token) {
+            state.token = token
+        },
+
+        destroyToken(state) {
+            state.token = null
         }
     },
 
@@ -156,6 +169,64 @@ export const store = new Vuex.Store({
                 .catch(error => {
                     console.log(error)
                 })
-        }
+        },
+
+        retrieveToken(context, credentials) {
+
+            return new Promise((resolve, reject) => {
+                axios.post('/login', {
+                    username: credentials.username,
+                    password: credentials.password,
+                  })
+                    .then(response => {
+                        console.log(response)
+                        const token = response.data.access_token
+          
+                        localStorage.setItem('access_token', token)
+                        context.commit('retrieveToken', token)
+                        resolve(response)
+                    })
+                    .catch(error => {
+                        reject(error)
+                    })
+            })
+        },
+
+        destroyToken(context) {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.token
+            if(context.getters.loggedIn) {
+                return new Promise((resolve, reject) => {
+                    axios.post('/logout')
+                        .then(response => {
+                            localStorage.removeItem('access_token')
+                            context.commit('destroyToken')
+                            resolve(response)
+                        })
+                        .catch(error => {
+                            localStorage.removeItem('access_token')
+                            context.commit('destroyToken')
+                            reject(error)
+                        })
+                })
+            }
+        },
+
+        register(context, data) {
+            return new Promise((resolve, reject) => {
+              axios.post('/register', {
+                name: data.name,
+                email: data.email,
+                password: data.password,
+              })
+                .then(response => {
+                  resolve(response)
+                })
+                .catch(error => {
+                  reject(error)
+                })
+            })
+        },
+
+              
     }
 })
